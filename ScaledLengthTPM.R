eef480e00e061e5da3259f680ddee32b5129e12a
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-09 14:06:54 +0200

    Update ScaledLengthTPM.R

--- ScaledLengthTPM.R
+++ ScaledLengthTPM.R
@@ -1,29 +1,52 @@
+# Set working directory
 setwd("E:/RNA Seq Data Analysis/siva_output_files")
+
+# Load libraries
 library(readr)
 library(tidyr)
+library(tximport)
+library(GenomicFeatures)
 
-library("tximport", lib.loc="~/R/win-library/3.5")
+# Get the list of directories
 dirs <- list.files("siva_SALMON_OUT/WT")
-dirs
-quant_files <- list.files("siva_SALMON_OUT/WT",pattern="quant.sf",recursive = TRUE,full.names = TRUE)
+print(dirs)
+
+# Get the list of quant files
+quant_files <- list.files("siva_SALMON_OUT/WT", pattern = "quant.sf", recursive = TRUE, full.names = TRUE)
 names(quant_files) <- dirs
-head(quant_files)
+print(head(quant_files))
+
+# Perform tximport with type = "salmon" and txOut = TRUE
 txi.tx <- tximport(quant_files, type = "salmon", txOut = TRUE)
+
+# Read the first quant file
 quants <- read_tsv(quant_files[1])
 
-library(GenomicFeatures)
+# Create txdb from gtf file
 gtf_file <- "transcriptome.gtf"
-file.exists(gtf_file)
-txdb <- makeTxDbFromGFF(gtf_file)
-keytypes(txdb)
-columns(txdb)
-k <- keys(txdb, keytype="TXNAME")
-
-library(tximport)
-tx_map <- select(txdb, keys = k, columns="GENEID", keytype = "TXNAME")
-head(tx_map)
-tx2gene <- tx_map
-write.csv(tx2gene,file="tx2gene-WT.csv",row.names = FALSE,quote=FALSE)
-
-txi_lengthscaledTPMvalues = tximport::tximport(quant_files,type = "salmon",txIn = TRUE, countsFromAbundance = "lengthScaledTPM",tx2gene = tx2gene,dropInfReps = TRUE)
-write.csv(txi_lengthscaledTPMvalues,file="txi_lengthscaledTPM_WT.csv",row.names = TRUE,quote=TRUE)
+if (file.exists(gtf_file)) {
+  txdb <- makeTxDbFromGFF(gtf_file)
+  print(keytypes(txdb))
+  print(columns(txdb))
+  
+  # Get keys from txdb
+  k <- keys(txdb, keytype = "TXNAME")
+  
+  # Generate tx2gene mapping
+  tx_map <- select(txdb, keys = k, columns = "GENEID", keytype = "TXNAME")
+  head(tx_map)
+  
+  # Save tx2gene mapping to a csv file
+  write.csv(tx_map, file = "tx2gene-WT.csv", row.names = FALSE, quote = FALSE)
+  
+  # Perform tximport with lengthScaledTPM
+  txi_lengthscaledTPMvalues <- tximport(quant_files, type = "salmon", txIn = TRUE,
+                                        countsFromAbundance = "lengthScaledTPM",
+                                        tx2gene = tx_map, dropInfReps = TRUE)
+  
+  # Save txi_lengthscaledTPMvalues to a csv file
+  write.csv(txi_lengthscaledTPMvalues, file = "txi_lengthscaledTPM_WT.csv",
+            row.names = TRUE, quote = TRUE)
+} else {
+  print("GTF file not found.")
+}
